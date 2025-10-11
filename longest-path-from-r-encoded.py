e86612f1369020b0007c138313104b5141b4092e
MatheusR42 <matheus.araujo.r42@gmail.com>
2025-10-11 18:51:16 -0300

    add get longest path

---
+++ longest-path-from-r-encoded.py
@@ -0,0 +1,38 @@
+rEncoded = {
+    "A": None,
+    "B": "A",
+    "C": "A",
+    "D": "B",
+    "E": "B",
+    "F": "C",
+    "G": "F"
+}
+
+def getShortestPath(encoded, s):
+    if s not in encoded:
+        return [None]
+
+    path = []
+    prev = encoded[s]
+
+    while prev:
+        path.append(prev)
+        prev = encoded[prev]
+
+    path.reverse()
+    path.append(s)
+    return path
+
+def getLongestPath(encoded):
+    max = 0
+
+    for v in encoded:
+        path = getShortestPath(encoded, v)
+        dist = len(path)
+
+        if dist > max:
+            max = dist
+        
+    return dist
+
+print('the shortest path is: ' + str(getLongestPath(rEncoded)))
